bed8897db491684e7570012d36149483664ab0a8
NetLSS <futureprogrammer@naver.com>
2021-06-29 15:34:08 +0900

    Feat: 네이버 맵 api 추가 (삽질 settings.gradle를 건드려야함)

--- app/src/main/java/com/lilcode/aop/p3c07/airbnb/MainActivity.kt
+++ app/src/main/java/com/lilcode/aop/p3c07/airbnb/MainActivity.kt
@@ -2,10 +2,14 @@ package com.lilcode.aop.p3c07.airbnb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.map.NaverMap
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+
 }
